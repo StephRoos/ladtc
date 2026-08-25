6d1136106a67839ea754232542a1dbcce9fd59cb
Stéphane Roos <stephaneroos@gmail.com>
2026-08-25 19:53:50 +0200

    fix: increase sponsor carousel scroll speed to 120px/s

--- src/components/sponsors/SponsorsCarousel.tsx
+++ src/components/sponsors/SponsorsCarousel.tsx
@@ -28,7 +28,7 @@ export function SponsorsCarousel({
   sponsors,
   title = "Nos sponsors",
   autoplay = true,
-  scrollSpeed = 80,
+  scrollSpeed = 120,
 }: SponsorsCarouselProps): React.ReactNode {
   const uniqueId = useId();
   const carouselId = `sponsors-carousel-${uniqueId}`;
